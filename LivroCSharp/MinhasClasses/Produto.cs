3edceaeb4851aed22fdd42072421b8f744fb534d
Sivaldo Oliveira <oliveira.smo@outlook.com>
2023-06-28 20:41:58 -0300

    Atualização 28/06/2023

---
+++ LivroCSharp/MinhasClasses/Produto.cs
@@ -0,0 +1,9 @@
+﻿namespace LivroCSharp.MinhasClasses
+{
+    public class Produto
+    {
+        public int Codigo { get; set; }
+        public string Nome { get; set; }
+        public decimal Preco { get; set; }
+    }
+}
